f3f71985cd3bb76ba69e494d7b205cf8036e35ab
Pankaj Pal <pankajpal197@gmail.com>
2024-02-09 17:30:07 +0530

    Background Change in React Project

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color,setColor]=useState("purple")
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor:color }} >
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 isset-x-0 px-2 ' >
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+          onClick={()=>setColor("red")}
+            className='bg-red-600 text-white px-4  py-2 rounded-full border border-red-200 font-semibold  text-sm hover:bg-red-500 '
+          >
+            Red
+          </button>
+          <button
+          onClick={()=>setColor("blue")}
+            className='bg-blue-600 text-white px-4  py-2 rounded-full border border-blue-200 font-semibold  text-sm hover:bg-blue-500 '
+          >
+            Blue
+          </button>
+          <button
+          onClick={()=>setColor("green")}
+            className='bg-green-600 text-white px-4  py-2 rounded-full border border-green-200 font-semibold  text-sm hover:bg-green-500 '
+          >
+            Green
+          </button>
+          <button
+          onClick={()=>setColor("purple")}
+            className='bg-purple-500 text-white px-4  py-2 rounded-full border border-purple-200 font-semibold  text-sm hover:bg-purple-800 '
+          >
+            Purple
+          </button>
+          <button
+          onClick={()=>setColor("yellow")}
+            className='bg-yellow-600 text-white px-4  py-2 rounded-full border border-yellow-200 font-semibold  text-sm hover:bg-yellow-500 '
+          >
+            Yellow
+          </button>
+          <button
+          onClick={()=>setColor("pink")}
+            className='bg-pink-600 text-white px-4  py-2 rounded-full border border-pink-200 font-semibold  text-sm hover:bg-pink-500 '
+          >
+            Pink
+          </button>
+          <button
+          onClick={()=>setColor("black")}
+            className='bg-black text-white px-4  py-2 rounded-full border border-black font-semibold  text-sm hover:bg-black '
+          >
+            Black
+          </button>
+          <button
+          onClick={()=>setColor("gray")}
+            className='bg-gray-600 text-white px-4  py-2 rounded-full border border-gray-200 font-semibold  text-sm hover:bg-gray-800 '
+          >
+            Gray
+          </button>
+          <button
+          onClick={()=>setColor("white")}
+            className='bg-white text-black px-4  py-2 rounded-full border border-white-800 font-semibold  text-sm hover:bg-white-200 '
+          >
+            White
+          </button>
+        </div>
+
+
+
+      </div>
+      {/* <div className='flex p-1  bg-red-100 gap-3 mx-auto text-center text-white shadow-lg'>
+      <button className='bg-red-500 w-12 p-3 ml-10 rounded-full  shadow-xs'>Red</button>
+      <button className='bg-red-500 px-3 ml-10 rounded-full  shadow-xs'>Red</button>
+    </div>
+   */}
+
+    </div>
+  )
+}
+
+export default App
